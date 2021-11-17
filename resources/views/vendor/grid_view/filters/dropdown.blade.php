3fdb4e6bc594efdb83cf64dd7298619cf7532427
Maulik Patel <93574359+maulikpatel93@users.noreply.github.com>
2021-11-17 10:32:58 +0530

    dsa

--- resources/views/vendor/grid_view/filters/dropdown.blade.php
+++ resources/views/vendor/grid_view/filters/dropdown.blade.php
@@ -3,7 +3,7 @@
     /** @var array $data */
     /** @var mixed $value onchange="if($(this).val()){$('#grid_view_filters_form').submit();}*/
 @endphp
-<select id="grid-filter-filters[{{ $name }}]" class="form-select grid-filter" name="filters[{{ $name }}]" role="grid-view-filter-item" form="grid_view_filters_form" onchange="if($(this).val()){$('#grid_view_filters_form').submit();}">
+<select id="grid-filter-filters[{{ $name }}]" class="form-select grid-filter" name="filters[{{ $name }}]" role="grid-view-filter-item" form="grid_view_filters_form" onchange="$('#grid_view_filters_form').submit();">
     <option value="">{!! trans('grid_view::grid.select') !!}</option>
     @foreach($data as $key => $val)
         <option value="{!! $key !!}" @if($value !== null && $value == $key) selected="selected" @endif >{!! $val !!}</option>
--- resources/views/vendor/grid_view/filters/text.blade.php
+++ resources/views/vendor/grid_view/filters/text.blade.php
@@ -2,4 +2,4 @@
 /** @var string $name */
 /** @var string $value onfocusout="if($(this).val()){$('#grid_view_filters_form').submit();}"*/
 @endphp
-<input type="text" id="grid-filter-filters[{{ $name }}]" class="form-control grid-filter" name="filters[{{ $name }}]" value="{{ $value }}" role="grid-view-filter-item" form="grid_view_filters_form" onfocusout="if($(this).val()){$('#grid_view_filters_form').submit();}"">
+<input type="text" id="grid-filter-filters[{{ $name }}]" class="form-control grid-filter" name="filters[{{ $name }}]" value="{{ $value }}" role="grid-view-filter-item" form="grid_view_filters_form" onfocusout="$('#grid_view_filters_form').submit();"">
