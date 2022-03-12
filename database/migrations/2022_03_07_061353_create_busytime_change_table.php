c27bedc44153bbe9d552dafb56557f6eaea961d2
Maulik Patel <93574359+maulikpatel93@users.noreply.github.com>
2022-03-12 12:43:22 +0530

    updated

--- database/migrations/2022_03_07_061353_create_busytime_change_table.php
+++ database/migrations/2022_03_07_061353_create_busytime_change_table.php
@@ -14,16 +14,14 @@ class CreateBusytimeChangeTable extends Migration
     public function up()
     {
         Schema::table('busy_time', function (Blueprint $table) {
-            $table->renameColumn('date', 'dateof');
-            $table->date('dateof')->change()->nullable();
+            $table->renameColumn('date', 'dateof')->change()->nullable();
             $table->integer('repeat_time', false, true)->nullable()->after('repeats')->comment('Week, Month');
             $table->enum('repeat_time_option', ['Weekly', 'Monthly', 'Yearly'])->after('repeat_time')->default(null)->nullable();
             $table->date('ending')->nullable()->after('repeat_time_option')->comment('Optional');
         });
 
         Schema::table('appointment', function (Blueprint $table) {
-            $table->renameColumn('date', 'dateof');
-            $table->date('dateof')->change()->nullable();
+            $table->renameColumn('date', 'dateof')->change()->nullable();
             $table->integer('repeat_time', false, true)->nullable()->after('repeats')->comment('Week, Month');
             $table->enum('repeat_time_option', ['Weekly', 'Monthly', 'Yearly'])->after('repeat_time')->default(null)->nullable();
             $table->date('ending')->nullable()->after('repeat_time_option')->comment('Optional');
