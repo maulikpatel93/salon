c6dc768bf684bea7ea5e738cd741003800ee25ad
Maulik Patel <93574359+maulikpatel93@users.noreply.github.com>
2022-04-22 13:17:38 +0530

    updated changes

--- database/migrations/2022_04_14_050013_create_addition_table.php
+++ database/migrations/2022_04_14_050013_create_addition_table.php
@@ -1,7 +1,6 @@
 <?php
 
 use Illuminate\Database\Migrations\Migration;
-use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
@@ -13,10 +12,7 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::table('subscription', function (Blueprint $table) {
-            $table->unsignedBigInteger('salon_id')->after('id')->nullable()->comment('Type Of Salon');
-            $table->foreign('salon_id')->references('id')->on('salons')->onUpdate('cascade')->onDelete('cascade');
-        });
+
     }
 
     /**
